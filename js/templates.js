e9daaae5ed7adf9ba9a57543046c9e57b4446dc3
irthompson01 <36970433+irthompson01@users.noreply.github.com>
2025-05-26 17:39:42 -0400

    hide blog and project dropdowns

--- js/templates.js
+++ js/templates.js
@@ -1,11 +1,47 @@
 // NovaStrada LLC Template System
 class NovaStradaTemplates {
+    // With Blog and Projects Dropdowns
+    // static navbar() {
+    //     return `
+    //         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    //             <div class="container px-5">
+    //                 <a class="navbar-brand" href="index.html">
+    //                     <img src="assets/logo_crop.PNG" alt="NovaStrada LLC" height="100" class="me-2">
+    //                 </a>
+    //                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+    //                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    //                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+    //                         <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+    //                         <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
+    //                         <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+    //                         <li class="nav-item"><a class="nav-link" href="services.html">Services</a></li>
+    //                         <li class="nav-item dropdown">
+    //                             <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Blog</a>
+    //                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
+    //                                 <li><a class="dropdown-item" href="blog-home.html">Blog Home</a></li>
+    //                                 <li><a class="dropdown-item" href="blog-post.html">Blog Post</a></li>
+    //                             </ul>
+    //                         </li>
+    //                         <li class="nav-item dropdown">
+    //                             <a class="nav-link dropdown-toggle" id="navbarDropdownProjects" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Projects</a>
+    //                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownProjects">
+    //                                 <li><a class="dropdown-item" href="projects.html">Projects Overview</a></li>
+    //                                 <li><a class="dropdown-item" href="project-detail.html">Project Details</a></li>
+    //                             </ul>
+    //                         </li>
+    //                     </ul>
+    //                 </div>
+    //             </div>
+    //         </nav>
+    //     `;
+    // }
+    // Without Blog and Projects Dropdowns
     static navbar() {
         return `
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container px-5">
                     <a class="navbar-brand" href="index.html">
-                        <img src="assets/logo_crop.PNG" alt="NovaStrada LLC" height="120" class="me-2">
+                        <img src="assets/logo_crop.PNG" alt="NovaStrada LLC" height="100" class="me-2">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -14,20 +50,6 @@ class NovaStradaTemplates {
                             <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
                             <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
                             <li class="nav-item"><a class="nav-link" href="services.html">Services</a></li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Blog</a>
-                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
-                                    <li><a class="dropdown-item" href="blog-home.html">Blog Home</a></li>
-                                    <li><a class="dropdown-item" href="blog-post.html">Blog Post</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="navbarDropdownProjects" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Projects</a>
-                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownProjects">
-                                    <li><a class="dropdown-item" href="projects.html">Projects Overview</a></li>
-                                    <li><a class="dropdown-item" href="project-detail.html">Project Details</a></li>
-                                </ul>
-                            </li>
                         </ul>
                     </div>
                 </div>
@@ -40,12 +62,8 @@ class NovaStradaTemplates {
             <footer class="bg-dark py-4 mt-auto">
                 <div class="container px-5">
                     <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-                        <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; NovaStrada LLC 2024</div></div>
+                        <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; NovaStrada LLC 2025</div></div>
                         <div class="col-auto">
-                            <a class="link-light small" href="#!">Privacy</a>
-                            <span class="text-white mx-1">&middot;</span>
-                            <a class="link-light small" href="#!">Terms</a>
-                            <span class="text-white mx-1">&middot;</span>
                             <a class="link-light small" href="contact.html">Contact</a>
                         </div>
                     </div>
